4ac9ce82b474978f6b9c98b21ced08a0298acffa
tygroo <tygroo972@free.fr>
2015-02-02 23:54:15 +0100

    ajout controle du user ajout fonction upload en fonction du user

--- src/main/java/GuiForm.java
+++ src/main/java/GuiForm.java
@@ -11,10 +11,12 @@ import javax.swing.*;
 import javax.swing.event.ListDataListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
+import javax.swing.text.html.HTMLEditorKit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by Bonheur on 05/01/2015.
@@ -31,18 +33,36 @@ public class GuiForm extends JFrame {
     private JCheckBox testCheckBox2;
     private JTable table1;
     private JTextPane textPane1;
+    private JTextArea textResult;
+    private JEditorPane editorPane1;
+    private JTextField userText;
+    private JPasswordField passwordText;
+    private JButton loginButton;
+    private JLabel userLabel;
+    private String content;
     DefaultListModel dlm;
     String[] columnNames;
     Object[][] data;
     DefaultTableModel model;
+    HTMLEditorKit editorKit;
+    String token;
+    String username;
+    String fullUser;
+
 
     public GuiForm() {
         super();
         File file = new File("");
         final Upload upl = new Upload();
 
+        content = "";
+        token = "";
+
         dlm = new DefaultListModel();
-        textPane1= new JTextPane();
+        //textPane1= new JTextPane();
+        editorKit = new HTMLEditorKit();
+        //textPane1.setEditorKit(editorKit);
+
         setContentPane(rootPanel);
 
         pack();
@@ -110,7 +130,7 @@ public class GuiForm extends JFrame {
                 }
                 if (testCheckBox2.isSelected()) {
                     url = "http://localhost:8081/rest/upload/file/";
-                    urlDL = "http://localhost:8080/rest/";
+                    urlDL = "http://localhost:8081/rest/";
                 }
 
 
@@ -123,7 +143,7 @@ public class GuiForm extends JFrame {
                     System.out.print("Debut du tranfert de :" + o);
                     String response = null;
                     try {
-                        response = upl.upload(url, file);
+                        response = upl.upload(url, file, token);
                     } catch (IOException e1) {
                         System.out.print("Erreur du tranfert de :" + o + "avec pour erreur :" + e1.getMessage());
                         //e1.printStackTrace();
@@ -144,9 +164,15 @@ public class GuiForm extends JFrame {
                             String shotNameMedium = (String) array.get("shortNameMed");
                             String shotNameLow = (String) array.get("shortNameLow");
 
-                            String content = namePict + "==> shotURL Normal :"+ urlDL+shotNameHight+ "|| shotURL Medium :"+ urlDL+shotNameMedium + "|| shotURL Low :"+ urlDL+shotNameLow;
-                            textPane1.add(new JTextArea(content,6,10));
-                            textPane1.setVisible(true);
+                            content += namePict + "==> \r   Normal : \r"+ urlDL+"short/"+shotNameHight+ " \r   Medium : \r"+ urlDL+"short/"+shotNameMedium + " \r   Low : \r"+ urlDL+"short/"+shotNameLow +" \n";
+                            //label.setText(content);
+                            //textResult.setText(content);
+
+                            editorPane1.setEditable(false);
+                                editorPane1.setText(content);
+                           // editorPane1.addHyperlinkListener();
+
+                            //textPane1.setVisible(true);
                             pack();
                             setVisible(true);
 
@@ -182,6 +208,49 @@ public class GuiForm extends JFrame {
                     pack();
                 }
 
+
+            }
+        });
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String urlDL = "http://198.27.66.107/cloudpix/rest/";
+                if (testCheckBox1.isSelected()) {
+                    urlDL = "http://localhost:8080/rest/";
+                }
+                if (testCheckBox2.isSelected()) {
+                    urlDL = "http://localhost:8081/rest/";
+                }
+
+                urlDL +="user/authenticate/";
+                String responseUsr = null;
+                String user = userText.getText();
+                String pwd = passwordText.getText();//getPassword().toString();
+
+                try {
+                    responseUsr = upl.authentication(urlDL, user, pwd);
+
+                } catch (IOException e1) {
+                    System.out.print("Erreur du login :" + userText.toString() + "avec pour erreur :" + e1.getMessage());
+                    //e1.printStackTrace();
+                }
+
+                JSONParser parser=new JSONParser();
+                Object obj= null;
+                try {
+                    obj = parser.parse(responseUsr);
+                    JSONObject array = (JSONObject) obj;
+
+                    fullUser = (String) array.get("token");
+
+                    username = fullUser.split(":")[0];
+                    token = fullUser.split(":")[1];
+
+                    userLabel.setText(username);
+                }catch (ParseException e1) {
+                    e1.printStackTrace();
+                }
 
             }
         });
--- src/main/java/Upload.java
+++ src/main/java/Upload.java
@@ -1,9 +1,13 @@
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import com.sun.jersey.multipart.FormDataBodyPart;
 import com.sun.jersey.multipart.FormDataMultiPart;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,10 +17,14 @@ import java.io.IOException;
  */
 public class Upload {
 
-    public String upload(String url, File uploadFile) throws IOException {
+    public String upload(String url, File uploadFile, String authtoken) throws IOException {
         WebResource resource = Client.create().resource(url);
         FormDataMultiPart form = new FormDataMultiPart();
         form.field("fileName", uploadFile.getName());
+        if(StringUtils.isNotBlank(authtoken)){
+            resource.header("X-Auth-Token",authtoken);
+        }
+
         FormDataBodyPart fdp = new FormDataBodyPart("file",
                 new FileInputStream(uploadFile),
                 MediaType.APPLICATION_OCTET_STREAM_TYPE);
@@ -24,4 +32,30 @@ public class Upload {
         String response = resource.type(MediaType.MULTIPART_FORM_DATA).post(String.class, form);
         return response;
     }
+    public String authentication(String url, String user, String pwd) throws IOException {
+        WebResource resource = Client.create().resource(url);
+//        FormDataMultiPart form = new FormDataMultiPart();
+//        resource.setProperty("username", user);
+  //      resource.setProperty("password", pwd);
+//        form.field("username", user);
+//        form.field("password", pwd);
+
+
+        MultivaluedMap<String, String> queryParams = new MultivaluedMapImpl();
+        queryParams.add("username", user); //set parametes for request
+        queryParams.add("password", pwd); //set parametes for request
+
+        ClientResponse response = null;
+        response = resource.queryParams(queryParams)
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .post(ClientResponse.class);
+
+        String jsonStr = response.getEntity(String.class);
+        //Get response from RESTful Server
+        System.out.println("Testing:");
+        System.out.println(jsonStr);
+//        String response = resource.type(MediaType.APPLICATION_JSON).post(String.class, form);
+        return jsonStr;
+    }
+
 }
